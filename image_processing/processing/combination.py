2b52ff09cfd8ba853ef143c9018de6cbb8215324
Danilo Leone <dlscarneiro@rd.com.br>
2024-10-29 19:49:12 -0300

    fix: Import module

--- image_processing/processing/combination.py
+++ image_processing/processing/combination.py
@@ -2,14 +2,14 @@ import numpy as np
 from skimage import io
 from skimage.color import rgb2gray
 from skimage.exposure import match_histograms
-from skimage.metrics import structural_similiraty
+from skimage.metrics import structural_similarity
 
 def find_difference(image1, image2):
     assert image1.shape == image2.shape, "Specify 2 images with de same shape."
 
     gray_image1 = rgb2gray(image1)
     gray_image2 = rgb2gray(image2)
-    (score, difference_image) = structural_similiraty(gray_image1, gray_image2, full=True)
+    (score, difference_image) = structural_similarity(gray_image1, gray_image2, full=True)
 
     print(f"Similarity of the images {score}")
     normalized_difference_image = (difference_image - np.min(difference_image)/(np.max(difference_image)-np.min(difference_image)))
